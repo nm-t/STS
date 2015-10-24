d28b367bf67d1bc7c349538d226c22472acb2dfa
loudang <ltdan2@student.monash.edu>
2015-10-24 16:42:09 +1100

    "conditions for validation set up (UI reaction not done)"

--- bin/webScript.js
+++ bin/webScript.js
@@ -2,32 +2,38 @@ $( document ).ready(function() {
 	var stageNum = 1;
 	console.log('is this even');
     $("#addStage").on('click', function() {
-    	console.log('called');
+    	
     	var newStage = "<li class='stage' ><h4>Stage " + stageNum + "</h4>Number of people <input type='number' class='people' min='0' style='width:50px'><br>Rule <input type='number' class='rule' min='0' style='width:50px'></li>"
         $("#trialSim").append(newStage);
         stageNum += 1
     });
     $("#start").on('click',function(){
-    		console.log("start_called")
+    		
     		var values = [];
-    		var peopleArray = $('.people').toArray().map(function(people) { return $(people).val(); });
-    		var ruleArray = $('.rule').toArray().map(function(rule) { return $(rule).val(); });
+    		var peopleArray = $('.people').toArray().map(function(people) { return parseInt($(people).val()); });
+    		var ruleArray = $('.rule').toArray().map(function(rule) { return parseInt($(rule).val()); });
     		var sumPeople = 0;
     		var i;
+    		allFieldOks = true;
     		for (i = 0; i<peopleArray.length; i++){
-    			if (peopleArray[i] === "" || ruleArray[i] === ""){
-    				var emptyBox = true;
+    			if (!peopleArray[i] || !ruleArray[i]){
+    				allFieldOk = false;
+    				//alert saying that a box is empty
     			}
     			sumPeople += peopleArray[i];
+    			
     			if (ruleArray[i]>sumPeople) {
-    				var ruleTooBig = true;
-    				//alert saying that the number of people to pass is greater than the total number
+    				allFieldsOk = false;
+    			    //alert saying that the number of people to pass is greater than the total number
     			}    			
     		}
+    		if (allFieldsOk){
+    			var inputData = R.zip(peopleArray, ruleArray);
+    			inputData = R.map(function (tuple) { return { numPeople: tuple[0], passThreshold: tuple[1] }; },inputData);
+				//at this point, we are ready to call createGrid on inputData
+			
+    		}
     		
-    		var inputData = R.zip(peopleArray, ruleArray);
-    		inputData = R.map(function (tuple) { return { numPeople: tuple[0], passThreshold: tuple[1] }; },inputData);
-			//at this point, we are ready to call createGrid on inputData
-						
+    					
     });
 });
