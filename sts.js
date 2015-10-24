ee7172e69bc2e6f90fad57dd9386392fda12332c
joashc <joash.is@gmail.com>
2015-10-24 14:44:38 +1100

    Added ctr given failure function

--- sts.js
+++ sts.js
@@ -43,9 +43,23 @@ var ctrGivenF = function (grid) {
     var cumulativePassRate = 0;
     var trueRateProportion = 1/grid.length;
 
-    grid.forEach(function (row) {
-        cumulativePassRate += (1 - row.passFraction) * trueRateProportion;
-        outputPairs.push( { trueRate: row.trueRate, passFraction: cumulativePassRate  });
+    var reversedGrid = R.reverse(grid);
+    var reversedWeights = R.reverse(weights);
+
+    var weightedRows = R.zip(reversedGrid, reversedWeights);
+
+    var bayesDenom = R.reduce(function (acc, weightedRow) {
+        var row = weightedRow[0];
+        var weight = weightedRow[1];
+        return acc + row.passFraction * weight;
+    }, 0, weightedRows);
+
+    var sumTrGivenS = 0;
+    weightedRows.forEach(function(weightedRow) {
+        var row = weightedRow[0];
+        var weight = weightedRow[1];
+        sumTrGivenS += ((1 - row.passFraction) * weight) / bayesDenom;
+        outputPairs.push({ trGivenS: sumTrGivenS, trueRate: row.trueRate });
     });
     return outputPairs;
 };
@@ -53,7 +67,6 @@ var ctrGivenF = function (grid) {
 var to2dArray = R.map(function (gridPoint) {
     return [gridPoint.trGivenS, gridPoint.trueRate];
 });
-
 
 var testData1 = [
     { trueRate: 0.1, passFraction: 0.15 },
@@ -67,11 +80,9 @@ var testData2 = [
     { trueRate: 0.3, passFraction: 0.7 }
 ];
 
-
 var grid =  createGrid([
             {numPeople: 1, passThreshold: 1},
             {numPeople: 5, passThreshold: 3 },
             {numPeople: 10, passThreshold: 5},
 ], [0.2, 0.4, 0.7]);
-console.log('stage=======', grid);
 console.log('sts', to2dArray(ctrGivenS(grid, [1, 1, 1])), grid);
