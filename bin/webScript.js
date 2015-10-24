976e20a1177a6262b13790b8b475dcffe35d4bb5
loudang <ltdan2@student.monash.edu>
2015-10-24 15:55:28 +1100

    parsing variables from HTML

--- bin/webScript.js
+++ bin/webScript.js
@@ -1,7 +1,14 @@
 $( document ).ready(function() {
-	var stageNum = 1
+	var stageNum = 1;
     $( "#addStage" ).click(function() {
-        $("#trialSim").append("<li><h4>Stage "+stageNum+"</h4><input type='number'><br><input type='number'></li>");
+    		var newStage = "<li class='stage' ><h4>Stage " + stageNum + "</h4>Number of people <input type='number' class='people' min='0' style='width:50px'><br>Rule <input type='number' class='rule' min='0' style='width:50px'></li>"
+        	$("#trialSim").append(newStage);
         stageNum += 1
+    });
+    $("#start").click(function(){
+    		var values = [];
+    		var peopleArray = $('.people').toArray().map(function(people) { return $(people).val(); });
+    		var ruleArray = $('.rule').toArray().map(function(rule) { return $(rule).val(); });
+    		var inputData = R.zip(peopleArray, ruleArray);
     });
 });
