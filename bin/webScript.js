a9ab163bad191a53895cf0b661fefb8b484bd243
loudang <ltdan2@student.monash.edu>
2015-10-24 17:02:31 +1100

    "webScript.js ready to integrate with the rest of the code (barring UI response for invalid inputs"

--- bin/webScript.js
+++ bin/webScript.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 	var stageNum = 1;
-	console.log('is this even');
+	
     $("#addStage").on('click', function() {
     	
     	var newStage = "<li class='stage' ><h4>Stage " + stageNum + "</h4>Number of people <input type='number' class='people' min='0' style='width:50px'><br>Rule <input type='number' class='rule' min='0' style='width:50px'></li>"
@@ -8,16 +8,17 @@ $( document ).ready(function() {
         stageNum += 1
     });
     $("#start").on('click',function(){
-    		
+    		//get values out of user input boxes
     		var values = [];
     		var peopleArray = $('.people').toArray().map(function(people) { return parseInt($(people).val()); });
     		var ruleArray = $('.rule').toArray().map(function(rule) { return parseInt($(rule).val()); });
     		var sumPeople = 0;
     		var i;
-    		allFieldOks = true;
+    		var allFieldsOk = true;
+    		//checking if any boxes are empty/if the rule exceeds the total participants at any point
     		for (i = 0; i<peopleArray.length; i++){
     			if (!peopleArray[i] || !ruleArray[i]){
-    				allFieldOk = false;
+    				allFieldsOk = false;
     				//alert saying that a box is empty
     			}
     			sumPeople += peopleArray[i];
@@ -28,9 +29,16 @@ $( document ).ready(function() {
     			}    			
     		}
     		if (allFieldsOk){
+    			console.log("things happened");
+    			//TODO get weights input from user, to weight the trials of different true effectiveness
     			var inputData = R.zip(peopleArray, ruleArray);
     			inputData = R.map(function (tuple) { return { numPeople: tuple[0], passThreshold: tuple[1] }; },inputData);
 				//at this point, we are ready to call createGrid on inputData
+				var grid = createGrid(inputData, interpolateRates(0, 1, 3));
+				//these three functions have optional parameter "weight[]"
+				//cumulateProb = ctr(grid);
+				data = ctrGivenS(grid);
+				//cumulateGivenFailure = ctrGivenF(grid);
 			
     		}
     		
