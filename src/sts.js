2066edcb822c0b5bc32270a19bb73bac8a9fc45c
Alejandro Dubrovsky <alitosis@gmail.com>
2015-10-25 13:43:52 +1100

    calculate data for the stages failed graph

--- src/sts.js
+++ src/sts.js
@@ -66,6 +66,43 @@ var ctrGivenF = function (grid, weights) {
     return outputPairs;
 };
 
+var endOfTrials = function (grid, weights) {
+    if (!weights) weights = R.repeat(1, grid.length);    
+    var outputSeries = {};
+
+    if (!grid) {
+        return {};
+    }
+
+    var sums = [];
+
+    // get the first row to find the number of stages
+    var first = grid[0];
+    var total_stages = first['stageFailed'].length + 1; // last one is the pass
+    for (var i=0; i < total_stages; i++) {
+        outputSeries[i] = [];
+        sums[i] = 0.0;
+    }
+    var weightedRows = R.zip(grid, weights);
+
+    weightedRows.forEach(function(weightedRow){
+        var row = weightedRow[0];
+        var weight = weightedRow[1];
+
+        row.stageFailed.forEach(function(failed, index) {
+            sums[index] += failed * weight;
+        });
+        sums[total_stages-1] = row.passFraction * weight;
+        total_at_stage = sums.reduce(function(a,b) { return a+b; }, 0);
+        for (var i=0; i < total_stages; i++) {
+            outputSeries[i].push([row.trueRate, sums[i] / total_at_stage]);
+        }
+
+    });
+
+    return outputSeries;
+};
+
 var to2dArray = R.map(function (gridPoint) {
     return [gridPoint.trueRate, gridPoint.passFraction];
 });
