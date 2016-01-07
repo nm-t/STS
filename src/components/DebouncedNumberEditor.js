e16875a2c6c66c54e716638017ca44c64625e8f4
joashc <joash.is@gmail.com>
2016-01-07 19:04:49 +1100

    Added debounced number editor
    Usually the number editor will call its update function every single
    time the value changes. If you drag fast enough, that can be dozens of
    times per second. This behaviour isn't ideal for us, because we
    recalculate all of the graph data every time the data changes, so this
    component wraps the number editor, so that it updates a local state
    instead of calling the update function.
    
    The update function is then debounced to only occur every x milliseconds.

---
+++ src/components/DebouncedNumberEditor.js
@@ -0,0 +1,51 @@
+/* @flow */
+import React, {Component, PropTypes } from 'react';
+import NumberEditor from 'react-number-editor';
+
+export default class DebouncedNumberEditor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: props.value, debouncing: false };
+    this.onLocalValueChange = this.onLocalValueChange.bind(this);
+  }
+
+  // $FlowIssue
+  static propTypes = {
+      onValueChange: PropTypes.func.isRequired,
+      value: PropTypes.number.isRequired,
+      min: PropTypes.number.isRequired,
+      max: PropTypes.number.isRequired,
+      label: PropTypes.string.isRequired,
+      step: PropTypes.number.isRequired,
+  };
+
+  onLocalValueChange(val: string) {
+    const newValue = parseInt(val);
+    const {min, max} = this.props;
+    if (newValue === this.state.value) return;
+    console.log(newValue, min, max, "FDSFDSFSFDS");
+    if (newValue < min) return;
+    if (newValue > max) return;
+    this.setState({value: newValue});
+    if (this.state.debouncing) return;
+    this.setState({debouncing: true});
+    setTimeout(() => {
+      this.props.onValueChange(this.state.value);
+      this.setState({debouncing: false})
+    }, 100);
+  }
+
+  render(): any {
+    const { onValueChange, min, max, label, step } = this.props;
+    return (<NumberEditor
+              step={step}
+              onValueChange={this.onLocalValueChange}
+              value={this.state.value}
+              min={min}
+              max={max}
+              style={{width: "2.5em"}}
+              label={label}
+           />);
+  }
+
+}
