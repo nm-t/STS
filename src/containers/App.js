39fd4e9c14847d77ab4e4c456f0147bb7f12ae5e
joashc <joash.is@gmail.com>
2016-01-07 16:48:24 +1100

    Got react transform boilerplate working
    Building off https://github.com/gaearon/react-transform-boilerplate/tree/4a2d0748f0a24913c82ffaf7cfb40a980cf6cc8c

---
+++ src/containers/App.js
@@ -0,0 +1,11 @@
+/* @flow */
+import React, { Component } from 'react';
+export default class App extends Component {
+  render(): any {
+    return (
+      <div>
+      STS
+      </div>
+    );
+  }
+}
