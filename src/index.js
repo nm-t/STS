3a1d35d3886d2b4562a72a49b4f74e177d40899e
joashc <joash.is@gmail.com>
2016-01-08 16:16:41 +1100

    Inject tap event plugin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './containers/App';
+import injectTapEventPlugin from "react-tap-event-plugin";
+injectTapEventPlugin();
 
 render(<App />, document.getElementById('root'));
