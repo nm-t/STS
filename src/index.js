5f67b64a12cfbfd979487b55a276a1e632bea1a2
joashc <joash.is@gmail.com>
2016-01-09 18:16:40 +1100

    Webpack the css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
+import '../css/nv.d3.min.css';
+import '../css/pure-min.css';
+import '../css/grids-responsive-min.css';
 import App from './containers/App';
 import injectTapEventPlugin from "react-tap-event-plugin";
 injectTapEventPlugin();
