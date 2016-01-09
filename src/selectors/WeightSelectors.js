b39503afcefbe9daad40cf0d2bdcf01de7b7a422
joashc <joash.is@gmail.com>
2016-01-09 21:39:54 +1100

    Hack to fix  ticks in end stage graph
    For some reason, forcing x-tick values doesn't work in nvd3 multibar charts,
    so we just show all the ticks, and then use the x-format function to
    only show the desired values. We coerce the desired x values into our
    interpolated true rates by interpolating between them instead of between
    0 and 1.

--- src/selectors/WeightSelectors.js
+++ src/selectors/WeightSelectors.js
@@ -1,6 +1,6 @@
 /* @flow */
 import { createSelector } from 'reselect';
-import { zipWith, assoc, scan, add, compose, map, curry, range } from 'ramda';
+import { flatten, aperture, zipWith, assoc, concat, sort, lt, scan, add, compose, map, curry, range } from 'ramda';
 import { distributionStateSelector } from './PriorDistSelectors';
 
 const interpolateRates = function(min: number, max: number, count: number): Array<number> {
@@ -13,14 +13,22 @@ const interpolateRates = function(min: number, max: number, count: number): Arra
     current += gap;
     rates[rates.length] = current;
   }
-  rates[rates.length] = max;
   return rates;
 };
 
+const setQuality = q => {
+  // Let's only use multiples of 10
+  const interpolateVal = Math.ceil(q / 10);
+  const minMaxes = aperture(2, [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]);
+  const interpolations = map(minMax => (interpolateRates(minMax[0], minMax[1], interpolateVal)), minMaxes);
+  return flatten([interpolations, 1]);
+};
+
 const log = (x) => { console.log(x); return x; };
+console.log(setQuality(30));
 
 // Hardcoding this for now, can make this configurable in future.
-export const trueRates = interpolateRates(0, 1, 30);
+export const trueRates = setQuality(100);
 
 export function round(x: number): number { return (Math.ceil(x * 1000) / 1000) };
 
