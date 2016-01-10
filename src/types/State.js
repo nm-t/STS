55897096dadbce7823f3fd49af645786f8fd57a1
joashc <joash.is@gmail.com>
2016-01-11 00:53:05 +1100

    Stop using object.freeze

--- src/types/State.js
+++ src/types/State.js
@@ -4,9 +4,6 @@ import Stage from './Stage';
 export default class State {
     constructor(stages: Array<Stage>) {
         this.stages = stages;
-
-        /// TODO: Don't use this in prod!
-        Object.freeze(this);
     }
     stages: Array<Stage>;
 }
